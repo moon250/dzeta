7bd7e52efcc847d201f6f60aa7c1ad92191075a6
moon250 <mooneupho@gmail.com>
2023-09-03 14:16:09 +0200

    feat: cache github api's data to reduce latency and avoid rate limiting

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\JsonApiResponse;
 use App\Services\GithubService;
+use Illuminate\Support\Facades\Cache;
 
 class GithubController extends Controller
 {
@@ -19,29 +20,57 @@ class GithubController extends Controller
 
     public function commits(): JsonApiResponse
     {
+        if (Cache::has('commits')) {
+            $commits = Cache::get('commits');
+        } else {
+            $commits = $this->service->commits();
+            Cache::set('commits', $commits, 30 * 60);
+        }
+
         return new JsonApiResponse([
-            'count' => $this->service->commits(),
+            'count' => (int) $commits,
         ]);
     }
 
     public function issues(): JsonApiResponse
     {
+        if (Cache::has('issues')) {
+            $issues = Cache::get('issues');
+        } else {
+            $issues = $this->service->issues();
+            Cache::set('issues', $issues, 30 * 60);
+        }
+
         return new JsonApiResponse([
-            'count' => $this->service->issues(),
+            'count' => (int) $issues,
         ]);
     }
 
     public function pullRequests(): JsonApiResponse
     {
+        if (Cache::has('pullRequests')) {
+            $pullRequests = Cache::get('pullRequests');
+        } else {
+            $pullRequests = $this->service->pullRequests();
+            Cache::set('pullRequests', $pullRequests, 30 * 60);
+        }
+
         return new JsonApiResponse([
-            'count' => $this->service->pullRequests(),
+            'count' => (int) $pullRequests,
         ]);
     }
 
     public function starred(): JsonApiResponse
     {
+        if (Cache::has('starred')) {
+            $starred = Cache::get('starred');
+        } else {
+            $starred = $this->service->starred();
+            Cache::set('starred', $starred, 30 * 60);
+        }
+
         return new JsonApiResponse([
-            'count' => $this->service->starred(),
+            'count' => (int) $starred,
         ]);
     }
 }
--- tests/Feature/Http/Controller/GithubControllerTest.php
+++ tests/Feature/Http/Controller/GithubControllerTest.php
@@ -3,6 +3,7 @@
 namespace Http\Controller;
 
 use App\Services\GithubService;
+use Illuminate\Support\Facades\Cache;
 use Mockery;
 use Mockery\MockInterface;
 use Tests\TestCase;
@@ -25,5 +26,89 @@ class GithubControllerTest extends TestCase
         $this->getJson('/github/pull-requests')->assertOk();
         $this->getJson('/github/starred')->assertOk();
         $this->getJson('/github/commits')->assertOk();
+    }
+
+    public function testIssuesAreCached()
+    {
+        Cache::spy();
+
+        $this->instance(
+            GithubService::class,
+            Mockery::mock(
+                GithubService::class,
+                function (MockInterface $mock) {
+                    $mock->shouldReceive('issues')->once();
+                }
+            )
+        );
+
+        $this->getJson('/github/issues')->assertOk();
+
+        Cache::shouldHaveReceived('set')
+            ->once()
+            ->with('issues', 0, 30 * 60);
+    }
+
+    public function testPullRequestsAreCached()
+    {
+        Cache::spy();
+
+        $this->instance(
+            GithubService::class,
+            Mockery::mock(
+                GithubService::class,
+                function (MockInterface $mock) {
+                    $mock->shouldReceive('pullRequests')->once();
+                }
+            )
+        );
+
+        $this->getJson('/github/pull-requests')->assertOk();
+
+        Cache::shouldHaveReceived('set')
+            ->once()
+            ->with('pullRequests', 0, 30 * 60);
+    }
+
+    public function testStarredAreCached()
+    {
+        Cache::spy();
+
+        $this->instance(
+            GithubService::class,
+            Mockery::mock(
+                GithubService::class,
+                function (MockInterface $mock) {
+                    $mock->shouldReceive('starred')->once();
+                }
+            )
+        );
+
+        $this->getJson('/github/starred')->assertOk();
+
+        Cache::shouldHaveReceived('set')
+            ->once()
+            ->with('starred', 0, 30 * 60);
+    }
+
+    public function testCommitsAreCache()
+    {
+        Cache::spy();
+
+        $this->instance(
+            GithubService::class,
+            Mockery::mock(
+                GithubService::class,
+                function (MockInterface $mock) {
+                    $mock->shouldReceive('commits')->once();
+                }
+            )
+        );
+
+        $this->getJson('/github/commits')->assertOk();
+
+        Cache::shouldHaveReceived('set')
+            ->once()
+            ->with('commits', 0, 30 * 60);
     }
 }
